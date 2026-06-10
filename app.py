7056c5cb31dd1401ad7d8aae03a1f70e6bd5bad1
ananyapuramshetty <ananyapuramshetty@gmail.com>
2026-06-10 12:29:13 +0530

    Implement Binary Search algorithm

--- app.py
+++ app.py
@@ -141,6 +141,29 @@ def linear_search(arr, target):
 
     return -1, comparisons
 
+def binary_search(arr, target):
+
+    left = 0
+    right = len(arr)-1
+
+    comparisons = 0
+
+    while left <= right:
+
+        comparisons += 1
+
+        mid = (left + right)//2
+
+        if arr[mid] == target:
+            return mid, comparisons
+
+        elif arr[mid] < target:
+            left = mid + 1
+
+        else:
+            right = mid - 1
+
+    return -1, comparisons
 
 @app.route("/", methods=["GET", "POST"])
 def home():
